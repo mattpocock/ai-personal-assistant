c579f508babb54e1f8394f088181eb3e6a6a7805
Matt Pocock <mattpocockvoice@gmail.com>
2025-11-11 16:19:24 +0000

    08.02.04 Added answerCorrectness Scorer

--- evals/e2e-retrieval.eval.ts
+++ evals/e2e-retrieval.eval.ts
@@ -4,6 +4,8 @@ import { google } from "@ai-sdk/google";
 import { convertToModelMessages, stepCountIs } from "ai";
 import { evalite } from "evalite";
 import { createUIMessageFixture } from "./create-ui-message-fixture";
+import { messageToText } from "@/app/utils";
+import { answerCorrectness } from "evalite/scorers";
 
 evalite.each([
   {
@@ -18,6 +20,16 @@ evalite.each([
       ),
       expected:
         "You bought a house at 42 Victoria Grove, Chorlton, Manchester M21 9EH.",
+    },
+    {
+      input: createUIMessageFixture<MyMessage>(
+        "What was the name of the person I was mentoring, and what was I mentoring them about?"
+      ),
+      expected: "You were mentoring Elena Kovac on the subject of climbing.",
+    },
+    {
+      input: createUIMessageFixture<MyMessage>("Am I married? If so, who to?"),
+      expected: "You are not married. Your partner is Alex Chen.",
     },
   ],
   task: async (input, model) => {
@@ -54,6 +66,19 @@ evalite.each([
     {
       label: "Expected",
       value: expected,
+    },
+  ],
+  scorers: [
+    {
+      scorer: ({ output, expected, input }) => {
+        return answerCorrectness({
+          question: input.map(messageToText).join("\n"),
+          answer: output.text,
+          reference: expected,
+          embeddingModel: google.textEmbeddingModel("text-embedding-004"),
+          model: google("gemini-2.5-flash-lite"),
+        });
+      },
     },
   ],
 });
