8e7c7f616b02f1e806d47de73e8787d986280386
Matt Pocock <mattpocockvoice@gmail.com>
2025-11-13 11:32:45 +0000

    10.01.01 Hook up Zapier MCP

--- src/app/api/chat/agent.ts
+++ src/app/api/chat/agent.ts
@@ -2,6 +2,7 @@ import {
   Experimental_Agent as Agent,
   LanguageModel,
   StopCondition,
+  ToolSet,
   UIMessage,
 } from "ai";
 import { MyMessage } from "./route";
@@ -27,10 +28,11 @@ export const createAgent = (opts: {
   stopWhen: StopCondition<any>;
   memories: DB.Memory[];
   relatedChats: DB.Chat[];
+  mcpTools: ToolSet;
 }) =>
   new Agent({
     model: opts.model,
-    tools: getTools(opts.messages),
+    tools: { ...getTools(opts.messages), ...opts.mcpTools },
     stopWhen: opts.stopWhen,
     system: `
 <task-context>
